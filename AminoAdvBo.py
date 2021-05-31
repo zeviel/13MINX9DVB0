258ab764a8662235d572c35ad418a36a5f14b26f
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-05-31 17:28:55 +0800

    Update AminoAdvBo.py

--- AminoAdvBo.py
+++ AminoAdvBo.py
@@ -5,13 +5,8 @@ init()
 print(Back.BLACK)
 print(Fore.RED)
 print(Style.NORMAL)
-print("""Script by Zevi/Скрипт сделан Zevi
-┌────────────────────────────────────┐
-│Author :  LilZevi                   │
-│Github : https://github.com/LilZevi │
-└────────────────────────────────────┘
-YouTube: https://www.youtube.com/channel/UCJ61JlXJckmO6yJr8BDRuGQ
-Telegram: @NowNameBo
+print("""Script by Lil Zevi
+Github : https://github.com/LilZevi
 ▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ ▄▀▄ █▀▄ ▐▌░▐▌ █▀▄ ▄▀▄
 █▀█ █░█░█ █ █░▀█ █░█ █▀█ █░█ ░▀▄▀░ █▀█ █░█
 ▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀░▀ ▀▀░ ░░▀░░ ▀▀░ ░▀░""")
