87787bb7770a104d360fa26e5a39813c8dc00ca9
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-08 20:53:26 +0600

    Update AminoAdvBo.py

--- AminoAdvBo.py
+++ AminoAdvBo.py
@@ -1,4 +1,5 @@
 import amino
+import asyncio
 import pyfiglet
 import concurrent.futures
 from colored import fore, back, style, attr
@@ -6,7 +7,7 @@ attr(0)
 print(fore.DARK_ORANGE + style.BOLD)
 print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
-print(pyfiglet.figlet_format("aminoadvbo", font="chunky"))
+print(pyfiglet.figlet_format("AminoAdvBo", font="jazmine"))
 lz = []
 def advertise(data):
     listusers = []
@@ -14,34 +15,33 @@ def advertise(data):
         listusers.append(userId)
     return listusers
 
-client = amino.Client()    
-email = input("Email >> ")
-password = input("Password >> ")
-msg = input("Message >> ")
-client.login(email=email, password=password)
-clients = client.sub_clients(start=0, size=1000)
-for x, name in enumerate(clients.name, 1):
-    print(f"{x}.{name}")
-communityid = clients.comId[int(input("Select the community >> "))-1]
-sub_client = amino.SubClient(comId=communityid, profile=client.profile)
-
-     
-        
-print("Sending Advertise")
-while True:
-	try:
-		users = sub_client.get_online_users(size=1000)
-		theusers = advertise(users)
-		for i in lz:
-			if i in theusers:
-				theusers.remove(i)
-		sub_client.start_chat(userId=theusers, message=msg)
-		with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-			_ = [executor.submit(sub_client.start_chat, theusers, msg) for userId in theusers]
-	except amino.lib.util.exceptions.VerificationRequired as e:
-		print(f"VerificationRequired")
-		link = e.args[0]['url']
-		print(link)
-		verify = input("Waiting for verification >> ")
-	except Exception as e:
-		print(e)
+async def main():
+	client = amino.Client()    
+	email = input("Email >> ")
+	password = input("Password >> ")
+	msg = input("Message >> ")
+	await client.login(email=email, password=password)
+	clients = await client.sub_clients(start=0, size=1000)
+	for x, name in enumerate(clients.name, 1):
+		print(f"{x}.{name}")
+	communityid = clients.comId[int(input("Select the community >> "))-1]
+	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
+	print("Sending Advertise")
+	while True:
+		try:
+			users = await sub_client.get_online_users(size=100)
+			theusers = advertise(users)
+			for i in lz:
+				if i in theusers:
+					theusers.remove(i)
+			await sub_client.start_chat(userId=theusers, message=msg)
+			await asyncio.gather(*[asyncio.create_task(sub_client.start_chat(theusers, msg)) for _ in range(45)])
+		except amino.lib.util.exceptions.VerificationRequired as e:
+			print(f"VerificationRequired")
+			link = e.args[0]['url']
+			print(link)
+			verify = input("Waiting for verification >> ")
+		except Exception as e:
+			print(e)
+			
+asyncio.get_event_loop().run_until_complete(main())
